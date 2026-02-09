9546339d2b5970c1e9f05b00809a4b0925dea333
kodejeet <roshansahx2003@gmail.com>
2026-02-09 23:23:58 +0530

    looping thru same pixel

--- main.c
+++ main.c
@@ -4,11 +4,14 @@
 
 int main() {
 
+  const int width = 900;
+  const int height = 600;
+
   printf("Hello World!");
   //this creates the window, only for a flash second 
   SDL_Window *pwindow = SDL_CreateWindow("Image Viewer",
-                              SDL_WINDOWPOS_CENTERED ,  SDL_WINDOWPOS_CENTERED , 900,
-                              600, 0 );
+                              SDL_WINDOWPOS_CENTERED ,  SDL_WINDOWPOS_CENTERED , width,
+                              height, 0 );
   // this keeps the window open for 3 second
   SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
@@ -18,11 +21,19 @@ int main() {
   Uint32 color = SDL_MapRGB(psurface->format,r,g,b);
 
   // creates a white background window
-  int x = 50; 
-  int y = 50;
+  // int x = 50; 
+  // int y = 50;
   //generated a single red pixel
-  SDL_Rect pixel = (SDL_Rect){x,y,1,1};
-  SDL_FillRect(psurface, &pixel, color);
+  
+  // this loops cover the whole screen of 900/600 red since the pixel runs on a loop
+  for(int x = 0; x<width; x++){
+    for (int y=0; y<height; y++) {
+      SDL_Rect pixel = (SDL_Rect){x,y,1,1};
+      SDL_FillRect(psurface, &pixel, color);
+    }
+  }
+  // SDL_Rect pixel = (SDL_Rect){x,y,1,1};
+  // SDL_FillRect(psurface, &pixel, color);
   SDL_UpdateWindowSurface(pwindow); 
   SDL_Delay(3000); 
 
